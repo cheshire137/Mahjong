d0b3a9b81534135051f6d2dd738c80bbf055f470
Sarah Vessels <cheshire137@gmail.com>
2016-08-20 10:32:36 -0500

    More Tile validations and tests

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,17 +1,33 @@
 class Tile < ApplicationRecord
+  WIND_NAMES = %w(east south west north).freeze
+  DRAGON_NAMES = %w(red green white).freeze
+
   validates :name, presence: true
   validates :category, presence: true
-  validates :number, presence: true, inclusion: 1..9, if: :suit?
 
-  def suit?
+  validates :number, presence: true, inclusion: 1..9, if: :suit_category?
+
+  validates :set, presence: true, if: :honor_category?
+  validates :name, inclusion: {in: WIND_NAMES}, if: :wind_tile?
+  validates :name, inclusion: {in: DRAGON_NAMES}, if: :dragon_tile?
+
+  def suit_category?
     category == 'suit'
   end
 
-  def honor?
+  def honor_category?
     category == 'honor'
   end
 
-  def flower?
+  def flower_category?
     category == 'flower'
+  end
+
+  def wind_tile?
+    honor_category? && set == 'wind'
+  end
+
+  def dragon_tile?
+    honor_category? && set == 'dragon'
   end
 end
--- test/models/tile_test.rb
+++ test/models/tile_test.rb
@@ -13,6 +13,12 @@ class TileTest < ActiveSupport::TestCase
     assert_includes tile.errors.keys, :category
   end
 
+  test 'requires set if honor category' do
+    tile = Tile.new(category: 'honor')
+    refute tile.valid?
+    assert_includes tile.errors.keys, :set
+  end
+
   test 'requires number if suit category' do
     tile = Tile.new(category: 'suit')
     refute tile.valid?
@@ -29,5 +35,29 @@ class TileTest < ActiveSupport::TestCase
     tile = Tile.new(category: 'suit', number: 10)
     refute tile.valid?
     assert_includes tile.errors.keys, :number
+  end
+
+  test 'suit_category? is true when category is suit' do
+    assert Tile.new(category: 'suit').suit_category?
+  end
+
+  test 'honor_category? is true when category is honor' do
+    assert Tile.new(category: 'honor').honor_category?
+  end
+
+  test 'flower_category? is true when category is flower' do
+    assert Tile.new(category: 'flower').flower_category?
+  end
+
+  test 'validates name for wind tiles' do
+    tile = Tile.new(name: 'whee', category: 'honor', set: 'wind')
+    refute tile.valid?
+    assert_includes tile.errors.keys, :name
+  end
+
+  test 'validates name for dragon tiles' do
+    tile = Tile.new(name: 'whee', category: 'honor', set: 'dragon')
+    refute tile.valid?
+    assert_includes tile.errors.keys, :name
   end
 end
