f0f6c8d3b149f7b9301bbf3ac43f5308eb3f80fe
Sarah Vessels <cheshire137@gmail.com>
2016-08-21 18:43:59 -0500

    Test can't access restricted endpoints when anonymous

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -35,4 +35,40 @@ class GamesControllerTest < ActionController::TestCase
       assert_redirected_to game_path(Game.last)
     end
   end
+
+  test 'cannot go to game page when not signed in' do
+    game = create(:game)
+    get :show, params: {id: game.id}
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'cannot shuffle when not signed in' do
+    game = create(:game)
+    process :shuffle, method: :post, params: {id: game.id}
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'cannot create a game when not signed in' do
+    assert_no_difference 'Game.count' do
+      process :create, method: :post
+      assert_redirected_to new_user_session_path
+    end
+  end
+
+  test 'cannot match tiles in an existing game when not signed in' do
+    game = build(:game)
+    game.initialize_tiles
+    assert game.save
+    before_value = game.tiles
+
+    tiles = before_value.split(';')
+    tile1 = tiles.first
+    tile1_id = tile1.split(':').first
+    tile2 = tiles.detect { |t| t != tile1 && t.split(':').first == tile1_id }
+
+    process :match, method: :post, params: {id: game.id,
+                                            tiles: "#{tile1};#{tile2}"}
+    assert_redirected_to new_user_session_path
+    assert_equal before_value, game.reload.tiles
+  end
 end
