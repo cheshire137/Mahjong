28a5f975e34cceea5f29be6a41a6636a68417d89
Sarah Vessels <cheshire137@gmail.com>
2016-08-21 12:07:56 -0500

    Don't call games JS unless we have a game board

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,246 +1,250 @@
-let selectedTile = null
-let resizeTimeout = false
+(function() {
+  let selectedTile = null
+  let resizeTimeout = false
 
-function hookUpTileLinks() {
-  const links = document.querySelectorAll('a.mahjong-tile.selectable')
-  links.forEach(link => {
-    link.addEventListener('click', selectTile)
-  })
-}
-
-function tileCoordinates(tile) {
-  const coordinates = tile.getAttribute('data-coords')
-  let [x, y, z] = coordinates.split(',')
-  x = parseInt(x, 10)
-  y = parseInt(y, 10)
-  z = parseInt(z, 10)
-  return { x, y, z }
-}
-
-function sortByCoordinates(a, b) {
-  const aCoords = tileCoordinates(a)
-  const bCoords = tileCoordinates(b)
-  if (aCoords.x > bCoords.x) {
-    return 1
+  function hookUpTileLinks() {
+    const links = document.querySelectorAll('a.mahjong-tile.selectable')
+    links.forEach(link => {
+      link.addEventListener('click', selectTile)
+    })
   }
-  if (aCoords.x < bCoords.x) {
-    return -1
-  }
-  if (aCoords.y > bCoords.y) {
-    return 1
-  }
-  if (aCoords.y < bCoords.y) {
-    return -1
-  }
-  return aCoords.z > bCoords.z ? 1 : -1
-}
 
-function tileToRight(tile, tiles) {
-  const coordinates = tileCoordinates(tile)
-  const toTheRight = tiles.filter(otherTile => {
-    const otherCoordinates = tileCoordinates(otherTile)
-    return otherCoordinates.x > coordinates.x &&
-        otherCoordinates.y === coordinates.y &&
-        otherCoordinates.z === coordinates.z
-  })[0]
-  return toTheRight
-}
+  function tileCoordinates(tile) {
+    const coordinates = tile.getAttribute('data-coords')
+    let [x, y, z] = coordinates.split(',')
+    x = parseInt(x, 10)
+    y = parseInt(y, 10)
+    z = parseInt(z, 10)
+    return { x, y, z }
+  }
 
-function tileBelow(tile, tiles) {
-  const coordinates = tileCoordinates(tile)
-  const below = tiles.filter(otherTile => {
-    const otherCoordinates = tileCoordinates(otherTile)
-    return otherCoordinates.x === coordinates.x &&
-        otherCoordinates.y > coordinates.y &&
-        otherCoordinates.z === coordinates.z
-  })[0]
-  return below
-}
-
-function boundedTile(tiles) {
-  const boundedTiles = tiles.filter(tile => {
-    const tileOnRight = tileToRight(tile, tiles)
-    const below = tileBelow(tile, tiles)
-    if (typeof tileOnRight === 'undefined') {
-      return false
+  function sortByCoordinates(a, b) {
+    const aCoords = tileCoordinates(a)
+    const bCoords = tileCoordinates(b)
+    if (aCoords.x > bCoords.x) {
+      return 1
     }
-    if (typeof below === 'undefined') {
-      return false
+    if (aCoords.x < bCoords.x) {
+      return -1
     }
+    if (aCoords.y > bCoords.y) {
+      return 1
+    }
+    if (aCoords.y < bCoords.y) {
+      return -1
+    }
+    return aCoords.z > bCoords.z ? 1 : -1
+  }
+
+  function tileToRight(tile, tiles) {
     const coordinates = tileCoordinates(tile)
-    const rightCoords = tileCoordinates(tileOnRight)
-    if ((rightCoords.x - coordinates.x) > 2) {
+    const toTheRight = tiles.filter(otherTile => {
+      const otherCoordinates = tileCoordinates(otherTile)
+      return otherCoordinates.x > coordinates.x &&
+          otherCoordinates.y === coordinates.y &&
+          otherCoordinates.z === coordinates.z
+    })[0]
+    return toTheRight
+  }
+
+  function tileBelow(tile, tiles) {
+    const coordinates = tileCoordinates(tile)
+    const below = tiles.filter(otherTile => {
+      const otherCoordinates = tileCoordinates(otherTile)
+      return otherCoordinates.x === coordinates.x &&
+          otherCoordinates.y > coordinates.y &&
+          otherCoordinates.z === coordinates.z
+    })[0]
+    return below
+  }
+
+  function boundedTile(tiles) {
+    const boundedTiles = tiles.filter(tile => {
+      const tileOnRight = tileToRight(tile, tiles)
+      const below = tileBelow(tile, tiles)
+      if (typeof tileOnRight === 'undefined') {
+        return false
+      }
+      if (typeof below === 'undefined') {
+        return false
+      }
+      const coordinates = tileCoordinates(tile)
+      const rightCoords = tileCoordinates(tileOnRight)
+      if ((rightCoords.x - coordinates.x) > 2) {
+        return false
+      }
+      const belowCoords = tileCoordinates(below)
+      if ((belowCoords.y - coordinates.y) > 2) {
+        return false
+      }
+      return true
+    })
+    return boundedTiles[0]
+  }
+
+  function adjustTileSizes() {
+    clearTimeout(resizeTimeout)
+    const tiles = Array.from(document.querySelectorAll('.mahjong-tile.is-visible'))
+    tiles.sort(sortByCoordinates)
+
+    // Ideal ratio:
+    const imageW = 37
+    const imageH = 46
+    const imageR = imageW / imageH
+
+    let scaledW = imageW
+    let scaledH = imageH
+
+    const constrainedTile = boundedTile(tiles)
+    const gameBoard = document.getElementById('game-board')
+
+    if (typeof constrainedTile === 'undefined') {
+      if (gameBoard.hasAttribute('data-tile-width') &&
+          gameBoard.hasAttribute('data-tile-height')) {
+        scaledW = gameBoard.getAttribute('data-tile-width')
+        scaledH = gameBoard.getAttribute('data-tile-height')
+      }
+    } else {
+      const rightSibling = tileToRight(constrainedTile, tiles)
+      const belowSibling = tileBelow(constrainedTile, tiles)
+
+      const tileRect = constrainedTile.getBoundingClientRect()
+      const rightRect = rightSibling.getBoundingClientRect()
+      const belowRect = belowSibling.getBoundingClientRect()
+
+      const availableW = rightRect.left - tileRect.left
+      const availableH = belowRect.top - tileRect.top
+      const availableR = availableW / availableH
+      if (availableR > imageR) {
+        scaledW = (imageW * availableH) / imageH
+        scaledH = availableH
+      } else {
+        scaledW = availableW
+        scaledH = (imageH * availableW) / imageW
+      }
+
+      gameBoard.setAttribute('data-tile-width', scaledW)
+      gameBoard.setAttribute('data-tile-height', scaledH)
+    }
+
+    tiles.forEach(tile => {
+      tile.style.width = `${scaledW}px`
+      tile.style.height = `${scaledH}px`
+
+      const coordinates = tileCoordinates(tile)
+      if (coordinates.z > 0) {
+        const image = tile.querySelector('.tile-image')
+        const factor = coordinates.z * 2
+        image.style.marginLeft = `-${factor}px`
+        image.style.marginTop = `-${factor}px`
+        image.style.boxShadow = `3px 3px 3px rgba(0, 0, 0, ${0.3 * coordinates.z})`
+      }
+
+      tile.classList.add('is-sized')
+    })
+  }
+
+  function tileProperties(tile) {
+    const coordinates = tile.getAttribute('data-coords')
+    const category = tile.getAttribute('data-category')
+    const set = tile.getAttribute('data-set')
+    const suit = tile.getAttribute('data-suit')
+    const number = tile.getAttribute('data-number')
+    const name = tile.getAttribute('data-name')
+    return { coordinates, category, set, suit, number, name }
+  }
+
+  function tileMatchProperties(tile) {
+    const id = tile.getAttribute('data-tile-id')
+    const coordinates = tile.getAttribute('data-coords')
+    const matchUrl = tile.getAttribute('data-match-url')
+    return { id, coordinates, matchUrl }
+  }
+
+  function isMatch(tileA, tileB) {
+    const propsA = tileProperties(tileA)
+    const propsB = tileProperties(tileB)
+    if (propsA.coordinates === propsB.coordinates) {
       return false
     }
-    const belowCoords = tileCoordinates(below)
-    if ((belowCoords.y - coordinates.y) > 2) {
+    if (propsA.category !== propsB.category) {
       return false
+    }
+    if (propsA.category === 'suit') {
+      if (propsA.suit !== propsB.suit) {
+        return false
+      }
+      if (propsA.number !== propsB.number) {
+        return false
+      }
+    }
+    if (propsA.set !== propsB.set) {
+      return false
+    }
+    if (propsA.set === 'wind' || propsA.set === 'dragon') {
+      if (propsA.name !== propsB.name) {
+        return false
+      }
     }
     return true
-  })
-  return boundedTiles[0]
-}
+  }
 
-function adjustTileSizes() {
-  clearTimeout(resizeTimeout)
-  const tiles = Array.from(document.querySelectorAll('.mahjong-tile.is-visible'))
-  tiles.sort(sortByCoordinates)
-
-  // Ideal ratio:
-  const imageW = 37
-  const imageH = 46
-  const imageR = imageW / imageH
-
-  let scaledW = imageW
-  let scaledH = imageH
-
-  const constrainedTile = boundedTile(tiles)
-  const gameBoard = document.getElementById('game-board')
-
-  if (typeof constrainedTile === 'undefined') {
-    if (gameBoard.hasAttribute('data-tile-width') &&
-        gameBoard.hasAttribute('data-tile-height')) {
-      scaledW = gameBoard.getAttribute('data-tile-width')
-      scaledH = gameBoard.getAttribute('data-tile-height')
+  function matchTiles(tile1, tile2) {
+    const props1 = tileMatchProperties(tile1)
+    const props2 = tileMatchProperties(tile2)
+    const gameBoard = document.getElementById('game-board')
+    const tiles = `${props1.id}:${props1.coordinates};${props2.id}:${props2.coordinates}`
+    const joiner = props1.matchUrl.indexOf('?') > -1 ? '&' : '?'
+    const url = `${props1.matchUrl}${joiner}tiles=${encodeURIComponent(tiles)}`
+    const data = {}
+    if (gameBoard.hasAttribute('data-tiles')) {
+      data.previous_tiles = gameBoard.getAttribute('data-tiles')
     }
-  } else {
-    const rightSibling = tileToRight(constrainedTile, tiles)
-    const belowSibling = tileBelow(constrainedTile, tiles)
+    $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'html',
+      data: data
+    }).done(response => {
+      gameBoard.innerHTML = response
+      hookUpTileLinks()
+      adjustTileSizes()
+    }).fail((jqXHR, status, error) => {
+      console.error('failed to match tiles', tiles, jqXHR, status, error)
+    })
+  }
 
-    const tileRect = constrainedTile.getBoundingClientRect()
-    const rightRect = rightSibling.getBoundingClientRect()
-    const belowRect = belowSibling.getBoundingClientRect()
-
-    const availableW = rightRect.left - tileRect.left
-    const availableH = belowRect.top - tileRect.top
-    const availableR = availableW / availableH
-    if (availableR > imageR) {
-      scaledW = (imageW * availableH) / imageH
-      scaledH = availableH
-    } else {
-      scaledW = availableW
-      scaledH = (imageH * availableW) / imageW
+  function selectTile(event) {
+    event.preventDefault()
+    const tile = event.currentTarget
+    if (selectedTile !== null) {
+      if (isMatch(selectedTile, tile)) {
+        matchTiles(selectedTile, tile)
+        return
+      } else {
+        selectedTile.classList.remove('is-selected')
+        selectedTile = null
+      }
     }
-
-    gameBoard.setAttribute('data-tile-width', scaledW)
-    gameBoard.setAttribute('data-tile-height', scaledH)
+    tile.classList.add('is-selected')
+    tile.blur()
+    selectedTile = tile
   }
 
-  tiles.forEach(tile => {
-    tile.style.width = `${scaledW}px`
-    tile.style.height = `${scaledH}px`
+  if (document.getElementById('game-board')) {
+    $(function() {
+      const csrfToken = document.querySelector('meta[name=csrf-token]').
+          getAttribute('content')
+      $.ajaxSetup({ headers: { 'X-CSRF-Token': csrfToken } })
+      hookUpTileLinks()
+      adjustTileSizes()
+    })
 
-    const coordinates = tileCoordinates(tile)
-    if (coordinates.z > 0) {
-      const image = tile.querySelector('.tile-image')
-      const factor = coordinates.z * 2
-      image.style.marginLeft = `-${factor}px`
-      image.style.marginTop = `-${factor}px`
-      image.style.boxShadow = `3px 3px 3px rgba(0, 0, 0, ${0.3 * coordinates.z})`
-    }
-
-    tile.classList.add('is-sized')
-  })
-}
-
-function tileProperties(tile) {
-  const coordinates = tile.getAttribute('data-coords')
-  const category = tile.getAttribute('data-category')
-  const set = tile.getAttribute('data-set')
-  const suit = tile.getAttribute('data-suit')
-  const number = tile.getAttribute('data-number')
-  const name = tile.getAttribute('data-name')
-  return { coordinates, category, set, suit, number, name }
-}
-
-function tileMatchProperties(tile) {
-  const id = tile.getAttribute('data-tile-id')
-  const coordinates = tile.getAttribute('data-coords')
-  const matchUrl = tile.getAttribute('data-match-url')
-  return { id, coordinates, matchUrl }
-}
-
-function isMatch(tileA, tileB) {
-  const propsA = tileProperties(tileA)
-  const propsB = tileProperties(tileB)
-  if (propsA.coordinates === propsB.coordinates) {
-    return false
+    $(window).resize(function() {
+      if (resizeTimeout) {
+        clearTimeout(resizeTimeout)
+      }
+      resizeTimeout = setTimeout(adjustTileSizes, 200)
+    })
   }
-  if (propsA.category !== propsB.category) {
-    return false
-  }
-  if (propsA.category === 'suit') {
-    if (propsA.suit !== propsB.suit) {
-      return false
-    }
-    if (propsA.number !== propsB.number) {
-      return false
-    }
-  }
-  if (propsA.set !== propsB.set) {
-    return false
-  }
-  if (propsA.set === 'wind' || propsA.set === 'dragon') {
-    if (propsA.name !== propsB.name) {
-      return false
-    }
-  }
-  return true
-}
-
-function matchTiles(tile1, tile2) {
-  const props1 = tileMatchProperties(tile1)
-  const props2 = tileMatchProperties(tile2)
-  const gameBoard = document.getElementById('game-board')
-  const tiles = `${props1.id}:${props1.coordinates};${props2.id}:${props2.coordinates}`
-  const joiner = props1.matchUrl.indexOf('?') > -1 ? '&' : '?'
-  const url = `${props1.matchUrl}${joiner}tiles=${encodeURIComponent(tiles)}`
-  const data = {}
-  if (gameBoard.hasAttribute('data-tiles')) {
-    data.previous_tiles = gameBoard.getAttribute('data-tiles')
-  }
-  $.ajax({
-    url: url,
-    type: 'POST',
-    dataType: 'html',
-    data: data
-  }).done(response => {
-    gameBoard.innerHTML = response
-    hookUpTileLinks()
-    adjustTileSizes()
-  }).fail((jqXHR, status, error) => {
-    console.error('failed to match tiles', tiles, jqXHR, status, error)
-  })
-}
-
-function selectTile(event) {
-  event.preventDefault()
-  const tile = event.currentTarget
-  if (selectedTile !== null) {
-    if (isMatch(selectedTile, tile)) {
-      matchTiles(selectedTile, tile)
-      return
-    } else {
-      selectedTile.classList.remove('is-selected')
-      selectedTile = null
-    }
-  }
-  tile.classList.add('is-selected')
-  tile.blur()
-  selectedTile = tile
-}
-
-$(function() {
-  const csrfToken = document.querySelector('meta[name=csrf-token]').
-      getAttribute('content')
-  $.ajaxSetup({ headers: { 'X-CSRF-Token': csrfToken } })
-  hookUpTileLinks()
-  adjustTileSizes()
-})
-
-$(window).resize(function() {
-  if (resizeTimeout) {
-    clearTimeout(resizeTimeout)
-  }
-  resizeTimeout = setTimeout(adjustTileSizes, 200)
-})
+})()
