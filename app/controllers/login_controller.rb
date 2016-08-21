d30d28d907022ce99a2432b161f0c8a95244331b
Sarah Vessels <cheshire137@gmail.com>
2016-08-21 17:29:56 -0500

    Simplify LoginController

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,15 +1,9 @@
 class LoginController < ApplicationController
-  before_action :redirect_if_signed_in
-
   def index
-    redirect_to temporary_games_path
-  end
-
-  private
-
-  def redirect_if_signed_in
     if user_signed_in?
       redirect_to games_path
+    else
+      redirect_to temporary_games_path
     end
   end
 end
