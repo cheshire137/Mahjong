7107027d89c5f88774bed71f1ec8ab800143aac7
Sarah Vessels <cheshire137@gmail.com>
2016-08-21 12:06:54 -0500

    Persist game after signing up
    Fixes #6.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    have_layout = (layout_id = session[:layout_id]) && Layout.exists?(layout_id)
+    have_tiles = (tiles = session[:tiles]).present?
+    if have_layout && have_tiles
+      game = Game.new(layout_id: layout_id, tiles: tiles, user: resource)
+      if game.save
+        session[:layout_id] = nil
+        session[:tiles] = nil
+        return game_path(game.id)
+      end
+    end
+    signed_in_root_path(resource)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
 
   resources :games, only: [:index, :show] do
     collection do
