899167cebf1c83510028e725f147c03ae1e3f1a6
Sarah Vessels <cheshire137@gmail.com>
2016-08-28 12:56:05 -0500

    Include shuffle_count, original_tiles in new game on register

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     have_layout = (layout_id = session[:layout_id]) && Layout.exists?(layout_id)
     have_tiles = (tiles = session[:tiles]).present?
     if have_layout && have_tiles
-      game = Game.new(layout_id: layout_id, tiles: tiles, user: resource)
+      game = Game.new(layout_id: layout_id, tiles: tiles, user: resource,
+                      shuffle_count: session[:shuffle_count],
+                      original_tiles: session[:original_tiles])
       if game.save
         session[:layout_id] = nil
         session[:tiles] = nil
+        session[:shuffle_count] = nil
+        session[:original_tiles] = nil
         return game_path(game.id)
       end
     end
