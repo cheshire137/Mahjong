86b82f26880669672ad1d9cf8f39145af7dd8fb0
Sarah Vessels <cheshire137@gmail.com>
2017-08-09 23:20:43 -0500

    Drop devise :trackable module from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, and
+  # :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   has_many :games
 end
